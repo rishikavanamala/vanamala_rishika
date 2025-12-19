6e327def6af2a3dc053e90beab9515ccd20212bb
rishikavanamala <124435431+rishikavanamala@users.noreply.github.com>
2025-12-19 19:34:39 +0530

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -1,1 +1,11 @@
 
+a = int(input("Enter a number: "))
+
+limit = a if a % 2 != 0 else a - 1
+
+odd_numbers = []
+
+for i in range(1, limit + 1, 2):
+    odd_numbers.append(i)
+
+print(", ".join(map(str, odd_numbers)))
